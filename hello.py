c1538daab9213bcb1db4391031543bba5e392e49
cai-yihua <jess92725@gmail.com>
2023-08-07 21:13:43 +0800

    firsr commit -3
        pick aa7a49b firsr commit -3
    yesung change

--- hello.py
+++ hello.py
@@ -1,10 +1,2 @@
-#number = input("請輸入數字：")
-#print("你輸入的數字是：", number)
-
-#try:
-#    number = input("請輸入數字：")
-#    print("你輸入的數字是：", number)
-#except (EOFError, KeyboardInterrupt):
-#    print("輸入錯誤：輸入已結束")
-
 print(input("請輸入數字："))
+print(input("Yesung is very cute !"))
